064ed15f2dca902826cb05e78f657a71ccf59b63
william swanson & Dana Sharman <student@epicodus.com>
2015-08-10 17:21:12 -0700

    test function incomplete

--- src/pingpong.php
+++ src/pingpong.php
@@ -4,8 +4,17 @@
     {
       function generatePingPongArray($number)
       {
+        $number = $_GET['number'];
+        $ping = ($_GET['number'] % 3);
 
+
+        if ($number = $ping) {
+            return "Ping";
+
+      } else {
+            return "this number is higher than 1, also good job!";
       }
 
+      }
     }
 ?>
